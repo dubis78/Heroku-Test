309c4b10beb5ad6a2185c500303308ba62c6e77c
dubis78 <43077233+dubis78@users.noreply.github.com>
2020-11-17 00:20:39 -0500

    test

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.use(express.json());
 //app.use(cors({origin: '*'}));
 app.use(cors({origin: 'https://kuepj-3000.sse.codesandbox.io'}));
 
+app.get('/',(req,res)=>{
+  res.json('hi');
+});
+
 // Routes//
 app.use('/api',users);
 
