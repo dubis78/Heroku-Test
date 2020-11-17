6a37b31fd3d90823346088a37455b9a2bd4a5176
dubis78 <43077233+dubis78@users.noreply.github.com>
2020-11-17 00:43:52 -0500

    cors all

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.set('port', process.env.PORT || 3001);
 
 app.use(express.json());
 
-//app.use(cors({origin: '*'}));
-app.use(cors({origin: 'https://kuepj-3000.sse.codesandbox.io'}));
+app.use(cors({origin: '*'}));
+//app.use(cors({origin: 'https://kuepj-3000.sse.codesandbox.io'}));
 
 app.get('/',(req,res)=>{
   res.send('hi');
